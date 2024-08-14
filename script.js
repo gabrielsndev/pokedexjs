1d9f1d4ca9ec21894b34191ff700e772d7accef4
gabreisndev <gabrielsn300@gmail.com>
2024-08-14 10:58:13 -0300

    testando usar o json

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 
-fetch("https://pokeapi.co/api/v2/pokemon/charmander")
-.then(response => {return(console.log(response.json()))})
+fetch("https://pokeapi.co/api/v2/pokemon/rattata")
+.then(response => response.json().then(data =>{
+    console.log(data);
+    console.log(data.name);
+    const h4 = document.getElementById("name")
+    h4.innerHTML = data.name
+}))
+.catch(err =>{
+    console.log(err);
+    window.alert('Aconteceu algum erro um erro: ' + err.message, 'Procure por outro Pokemon ')
+
+})
