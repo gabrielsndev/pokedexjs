a713f98a648db1e0bfcd2a06670b8d9dffdc8d8a
gabreisndev <gabrielsn300@gmail.com>
2024-08-16 10:59:24 -0300

    começando uma base pra inserir informações com javascript

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-/*fetch("https://pokeapi.co/api/v2/pokemon/rattata")
+fetch("https://pokeapi.co/api/v2/pokemon/rattata")
 .then(response => response.json().then(data =>{
     console.log(data);
     console.log(data.name);
     const main = document.getElementsByClassName("main")[0]
-    main.innerHTML =  `<img src="${data['sprites']['front_default']}" alt="${data.name}" width="200" height="200">`
+/*    main.innerHTML =  `<img src="${data['sprites']['front_default']}" alt="${data.name}" width="200" height="200">`*/
 }))
 .catch(err =>{
     console.log(err);
     window.alert('Aconteceu algum erro um erro: ' + err.message, 'Procure por outro Pokemon ')
 
-})*/
+})
+
