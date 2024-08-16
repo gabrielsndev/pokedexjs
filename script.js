95c7371d901d00f8d1957201d933f1b064741d34
gabreisndev <gabrielsn300@gmail.com>
2024-08-15 21:34:53 -0300

    terminando os estilos mobile

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-/*
-fetch("https://pokeapi.co/api/v2/pokemon/rattata")
+/*fetch("https://pokeapi.co/api/v2/pokemon/rattata")
 .then(response => response.json().then(data =>{
     console.log(data);
     console.log(data.name);
-    const h4 = document.getElementById("name")
-    h4.innerHTML = data.name
+    const main = document.getElementsByClassName("main")[0]
+    main.innerHTML =  `<img src="${data['sprites']['front_default']}" alt="${data.name}" width="200" height="200">`
 }))
 .catch(err =>{
     console.log(err);
